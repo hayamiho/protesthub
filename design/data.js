4201d44f2e334d8ae1e69ee995f056028a19cd7e
hayamiho <mack.harry.222@gmail.com>
2026-07-21 02:22:43 +0900

    サムネイル表示を変更

--- design/data.js
+++ design/data.js
@@ -1,6 +1,7 @@
 const POSTERS_DATA = [
     {
         "file": "pl-000001-a.png",
+        "set": "1",
         "title": "反戦",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -16,6 +17,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000001-b.png",
+        "set": "1",
         "title": "反戦",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -31,6 +33,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000001-c.png",
+        "set": "1",
         "title": "反戦",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -46,6 +49,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000002-a.png",
+        "set": "1",
         "title": "NO WAR",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "反戦",
@@ -61,6 +65,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000002-b.png",
+        "set": "1",
         "title": "NO WAR",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "反戦",
@@ -76,6 +81,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000002-c.png",
+        "set": "1",
         "title": "NO WAR",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "反戦",
@@ -91,6 +97,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000003-a.png",
+        "set": "1",
         "title": "高市首相、減税まだ？",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "税",
@@ -106,6 +113,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000003-b.png",
+        "set": "1",
         "title": "高市首相、減税まだ？",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "税",
@@ -121,6 +129,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000003-c.png",
+        "set": "1",
         "title": "高市首相、減税まだ？",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "税",
@@ -136,6 +145,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000004-a.png",
+        "set": "1",
         "title": "NOWAR",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -151,6 +161,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000004-b.png",
+        "set": "1",
         "title": "NOWAR",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -166,6 +177,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000004-c.png",
+        "set": "1",
         "title": "NOWAR",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -181,6 +193,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000005-a.png",
+        "set": "1",
         "title": "戦争反対",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -196,6 +209,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000005-b.png",
+        "set": "1",
         "title": "戦争反対",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -211,6 +225,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000005-c.png",
+        "set": "1",
         "title": "戦争反対",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "反戦",
@@ -226,6 +241,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000006-a.png",
+        "set": "",
         "title": "国旗損壊罪 いらない！",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "政治",
@@ -241,6 +257,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000006-b.png",
+        "set": "",
         "title": "武器輸出 やめて!!",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "反戦",
@@ -256,6 +273,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000006-c.png",
+        "set": "",
         "title": "減税 早くして!",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "税",
@@ -271,6 +289,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000008-a.png",
+        "set": "",
         "title": "◯◯いらない!",
         "desc": "上半分に書きこめます。4200×2970px　A3/A4兼用",
         "cat": "政治",
@@ -286,6 +305,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000008-b.png",
+        "set": "",
         "title": "◯◯やめて!!",
         "desc": "上半分に書きこめます。4200×2970px　A3/A4兼用",
         "cat": "政治",
@@ -301,6 +321,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000008-c.png",
+        "set": "",
         "title": "◯◯早くして!",
         "desc": "上半分に書きこめます。4200×2970px　A3/A4兼用",
         "cat": "政治",
@@ -316,6 +337,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000009-a.png",
+        "set": "1",
         "title": "議員定数 減らすな!",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "法案",
@@ -331,6 +353,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000009-b.png",
+        "set": "1",
         "title": "議員定数 減らすな!",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "法案",
@@ -346,6 +369,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000009-c.png",
+        "set": "1",
         "title": "議員定数 減らすな!",
         "desc": "4200×2970px　A3/A4兼用",
         "cat": "法案",
@@ -361,6 +385,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000010-a.png",
+        "set": "",
         "title": "自民党の改憲草案 読んでみませんか？",
         "desc": "3580×5033px（高解像度）<br/><a href=\"https://kaikensouan.com/\" target=\"_blank\">改憲草案ドットコム</a>紹介プラカードです。<br/>制作意図に反するものでなければ書き込みも自由です。<br/>（戦争賛美・差別的なこと・紹介しているサイトへの批判 などはNGです）",
         "cat": "憲法",
@@ -376,6 +401,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000010-b.png",
+        "set": "",
         "title": "自民党の改憲草案 読んでみませんか？",
         "desc": "5033×3580px（高解像度）<br/><a href=\"https://kaikensouan.com/\" target=\"_blank\">改憲草案ドットコム</a>紹介プラカードです。<br/>制作意図に反するものでなければ書き込みも自由です。<br/>（戦争賛美・差別的なこと・紹介しているサイトへの批判 などはNGです）",
         "cat": "憲法",
@@ -391,6 +417,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000011-a.png",
+        "set": "",
         "title": "デモカレンダー 本日の開催地＋開催数のお知らせ",
         "desc": "3509×4961px（高解像度）<br/><a href=\"https://democalendar.jp/\" target=\"_blank\">デモカレンダー</a>紹介プラカードです。<br/>制作意図に反するものでなければ書き込みも自由です。<br/>（戦争賛美・差別的なこと・紹介しているサイトへの批判 などはNGです）",
         "cat": "その他",
@@ -406,6 +433,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000012-a.png",
+        "set": "",
         "title": "私たちは悪政に屈しない。",
         "desc": "3508×4961px（高解像度）<br/>制作意図に反するものでなければ書き込みも自由です。<br/>（戦争賛美・差別的なこと・紹介しているサイトへの批判 などはNGです）",
         "cat": "反戦",
@@ -422,6 +450,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000012-b.png",
+        "set": "",
         "title": "平和国家を過去にしないで。",
         "desc": "3509×4961px（高解像度）<br/>制作意図に反するものでなければ書き込みも自由です。<br/>（戦争賛美・差別的なこと・紹介しているサイトへの批判 などはNGです）",
         "cat": "反戦",
@@ -438,6 +467,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000012-c.png",
+        "set": "",
         "title": "Don't let PEACE become the past.",
         "desc": "3508×4961px（高解像度）<br/>制作意図に反するものでなければ書き込みも自由です。<br/>（戦争賛美・差別的なこと・紹介しているサイトへの批判 などはNGです）",
         "cat": "反戦",
@@ -454,6 +484,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000013-a.png",
+        "set": "",
         "title": "せんそうはんたい！こどもをまもれ！",
         "desc": "3005×4250px（高解像度）<br/>「英語のカッコいいプラカードももちろんいいけど、子どもも読めるデザインのものもあるといいよね」というご意見を頂戴して作りました。ひらがなプラカ広まれ！",
         "cat": "反戦",
@@ -471,6 +502,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000013-b.png",
+        "set": "",
         "title": "せかいのみんなとなかよくしよう！",
         "desc": "3005×4250px（高解像度）<br/>「英語のカッコいいプラカードももちろんいいけど、子どもも読めるデザインのものもあるといいよね」というご意見を頂戴して作りました。ひらがなプラカ広まれ！",
         "cat": "反戦",
@@ -488,6 +520,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000014-a.png",
+        "set": "",
         "title": "ひとの税金で武器売買すな！",
         "desc": "3536×5000px（高解像度）",
         "cat": "反戦",
@@ -504,6 +537,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000014-b.png",
+        "set": "",
         "title": "ウチらで平和憲法まもろ！",
         "desc": "3536×5000px（高解像度）<br/>現在の日本国憲法が政治権力を縛り、平和に寄与していることを伝えたく「\"平和\"憲法」という表現にしました。政府の意向にむやみに反対しているのではなく、平和を守りたい一心での言葉であり、主権者はあくまで私たち市民であることを「ウチら」に込めています。",
         "cat": "反戦",
@@ -520,6 +554,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000015-a.png",
+        "set": "",
         "title": "家族（ペット）の命 戦争に奪わせないで",
         "desc": "2970×4200px　A3/A4兼用<br/>ナフサ不足によりペット用の医療器具も不足しはじめています。戦争は弱いものから搾取されていくようになっているので、大切な家族（ペット）を守りたいという思いから作りました。",
         "cat": "反戦",
@@ -536,6 +571,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000016-a.jpg",
+        "set": "",
         "title": "まもって！憲法　NO WAR",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "憲法",
@@ -553,6 +589,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000016-b.jpg",
+        "set": "",
         "title": "まもって！憲法　消さないで 基本的人権",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "憲法",
@@ -570,6 +607,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000017-a.jpg",
+        "set": "",
         "title": "主権者はウチら！",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "政治",
@@ -585,6 +623,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000017-b.jpg",
+        "set": "",
         "title": "国のコト決めるのはウチら！",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "政治",
@@ -600,6 +639,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000017-c.jpg",
+        "set": "",
         "title": "政治を変える手段って、選挙だけじゃない！",
         "desc": "2970×4200px　A3/A4兼用",
         "cat": "政治",
@@ -615,6 +655,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000018-a.jpg",
+        "set": "",
         "title": "デモって、ウチらの声をそのまま届ける手段だから大事なんだね♡",
         "desc": "4250×3005px（高解像度）",
         "cat": "政治",
@@ -630,6 +671,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000018-b.jpg",
+        "set": "",
         "title": "人生初デモのひと、いま沢山います。",
         "desc": "4250×3005px（高解像度）",
         "cat": "政治",
@@ -645,6 +687,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000019-a.png",
+        "set": "",
         "title": "戦争イヤなら、ごいっしょに！飛びこみOK　手ぶらでOK　立ってるだけでも大丈夫",
         "desc": "4250×3005px（高解像度）",
         "cat": "政治",
@@ -660,6 +703,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000019-b.png",
+        "set": "",
         "title": "立ってるだけでも大丈夫！　飛びこみOK　手ぶらでOK　5分だけでも大歓迎",
         "desc": "4250×3005px（高解像度）",
         "cat": "政治",
@@ -675,6 +719,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000020-a.png",
+        "set": "",
         "title": "次のデモは いつ・どこで？そんな時はデモカレンダー！",
         "desc": "3496×4961px（高解像度）<br/>【<a href=\"https://democalendar.jp/\" target=\"_blank\">デモカレンダー</a>】をお知らせするプラカードを作りました。パッと読めるように情報量は少なめ／QRコードでかめにしました。複数枚設置できるシッティングと相性いいかも！<br/>セブンイレブン ネップリ～8/4：3YBTE3BB",
         "cat": "その他",
@@ -690,6 +735,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000020-b.png",
+        "set": "",
         "title": "次のデモは いつ・どこで？そんな時はデモカレンダー！",
         "desc": "3496×4961px（高解像度）<br/>【<a href=\"https://democalendar.jp/\" target=\"_blank\">デモカレンダー</a>】をお知らせするプラカードを作りました。パッと読めるように情報量は少なめ／QRコードでかめにしました。複数枚設置できるシッティングと相性いいかも！<br/>セブンイレブン ネップリ～8/4：Y47LRR2C",
         "cat": "その他",
@@ -705,6 +751,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000020-c.png",
+        "set": "",
         "title": "次のデモは いつ・どこで？そんな時はデモカレンダー！",
         "desc": "3496×4961px（高解像度）<br/>【<a href=\"https://democalendar.jp/\" target=\"_blank\">デモカレンダー</a>】をお知らせするプラカードを作りました。パッと読めるように情報量は少なめ／QRコードでかめにしました。複数枚設置できるシッティングと相性いいかも！<br/>セブンイレブン ネップリ～8/4：6QB73SZL",
         "cat": "その他",
@@ -720,6 +767,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000021-a.jpg",
+        "set": "",
         "title": "I HOPE WORLD PEACE",
         "desc": "4200×2970px　A3/A4兼用<br/>・反戦、平和を伝える事が彼の目的です。<br/>・注意事項：著作権放棄してません。二次配布、キャラクターイメージを著しく損なうような表現・改変の禁止。AI学習禁止。営利目的での利用禁止。政党、過激派組織、宗教団体による利用禁止。特定の政党を支持させる目的での利用禁止。",
         "cat": "反戦",
@@ -738,6 +786,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000021-b.jpg",
+        "set": "",
         "title": "デモってなんか怖くね？俺は人権と命がおびやかされる方が怖いゼ！",
         "desc": "2970×4200px　A3/A4兼用<br/>・反戦、平和を伝える事が彼の目的です。<br/>・注意事項：著作権放棄してません。二次配布、キャラクターイメージを著しく損なうような表現・改変の禁止。AI学習禁止。営利目的での利用禁止。政党、過激派組織、宗教団体による利用禁止。特定の政党を支持させる目的での利用禁止。",
         "cat": "反戦",
@@ -757,6 +806,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000021-c.jpg",
+        "set": "",
         "title": "せんそう やめよ！",
         "desc": "4200×2970px　A3/A4兼用<br/>・反戦、平和を伝える事が彼の目的です。<br/>・注意事項：著作権放棄してません。二次配布、キャラクターイメージを著しく損なうような表現・改変の禁止。AI学習禁止。営利目的での利用禁止。政党、過激派組織、宗教団体による利用禁止。特定の政党を支持させる目的での利用禁止。",
         "cat": "反戦",
@@ -776,6 +826,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000022-a.png",
+        "set": "",
         "title": "YES PEACE",
         "desc": "3536×5000px（高解像度）<br/>A3対応の<a href=\"https://www.protesthub.jp/design/images/pl-000022-a.pdf\" target=\"_blank\">PDFデータ</a>もあります。",
         "cat": "反戦",
@@ -792,6 +843,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000022-b.png",
+        "set": "",
         "title": "LOVE PEACE",
         "desc": "3536×5000px（高解像度）<br/>A3対応の<a href=\"https://www.protesthub.jp/design/images/pl-000022-b.pdf\" target=\"_blank\">PDFデータ</a>もあります。",
         "cat": "反戦",
@@ -808,6 +860,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "pl-000022-c.png",
+        "set": "",
         "title": "LOVE PEACE",
         "desc": "3536×5000px（高解像度）<br/>A3対応の<a href=\"https://www.protesthub.jp/design/images/pl-000022-c.pdf\" target=\"_blank\">PDFデータ</a>もあります。",
         "cat": "反戦",
@@ -824,6 +877,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "fl-000001-a.png",
+        "set": "1",
         "title": "Q.どうして野党は反対ばっかりするの？ A.与党が問題のある法案ばかり通すからです。",
         "desc": "2970×4200px　A3/A4兼用<br/>自宅やコンビニ印刷・印刷所の入稿・メルカリ出品もOK。ポスティングやデモでの配布にお役立てください。<br/>PDF＋トンボつき入稿PDFは<a href=\"https://note.com/schwarzewald/n/nf95c5ff2f893\" target=\"_blank\">note</a>から。<br/>関連タグ：<a href=\"https://x.com/hashtag/%E8%87%AA%E6%B0%91%E5%85%9A%E7%84%A1%E7%90%86%E3%81%99%E3%81%8E%E3%83%9D%E3%82%B9%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E9%83%A8?src=hashtag_click\" target=\"_blank\">#自民党無理すぎポスティング部</a>",
         "cat": "政治",
@@ -843,6 +897,7 @@ const POSTERS_DATA = [
     },
     {
         "file": "fl-000001-b.png",
+        "set": "1",
         "title": "いま国会で問題になっている主な法案と、その問題点",
         "desc": "2970×4200px　A3/A4兼用<br/>＜注：こちらは裏面です＞<br/>自宅やコンビニ印刷・印刷所の入稿・メルカリ出品もOK。ポスティングやデモでの配布にお役立てください。<br/>PDF＋トンボつき入稿PDFは<a href=\"https://note.com/schwarzewald/n/nf95c5ff2f893\" target=\"_blank\">note</a>から。<br/>関連タグ：<a href=\"https://x.com/hashtag/%E8%87%AA%E6%B0%91%E5%85%9A%E7%84%A1%E7%90%86%E3%81%99%E3%81%8E%E3%83%9D%E3%82%B9%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E9%83%A8?src=hashtag_click\" target=\"_blank\">#自民党無理すぎポスティング部</a>",
         "cat": "政治",
@@ -857,6 +912,38 @@ const POSTERS_DATA = [
         ],
         "by": "schwarzewald",
         "url": "https://x.com/schwarzewald",
+        "og_title": "",
+        "og_desc": ""
+    },
+    {
+        "file": "pl-000023-a.png",
+        "set": "",
+        "title": "めちゃくちゃな政治に抗議するために仕事終わりにデモに来ている",
+        "desc": "2970×4200px　A3/A4兼用",
+        "cat": "その他",
+        "tags": [
+            "デモ",
+            "日本語",
+            "縦"
+        ],
+        "by": "sue_syrup",
+        "url": "https://x.com/sue_syrup",
+        "og_title": "",
+        "og_desc": ""
+    },
+    {
+        "file": "pl-000023-b.png",
+        "set": "",
+        "title": "めちゃくちゃな政治に抗議するために趣味の合間にスタンディング",
+        "desc": "2970×4200px　A3/A4兼用",
+        "cat": "その他",
+        "tags": [
+            "デモ",
+            "日本語",
+            "縦"
+        ],
+        "by": "sue_syrup",
+        "url": "https://x.com/sue_syrup",
         "og_title": "",
         "og_desc": ""
     }
